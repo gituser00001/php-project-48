d7e8cd290c47baa48c7d86a999e39b63deed0963
gituser00001 <lptvsergey@yandex.ru>
2022-09-28 22:42:11 +0300

    fix file name

--- src/Parsers.php
+++ src/Parsers.php
@@ -9,8 +9,7 @@ function parseData(string $file, string $extension): array
     switch ($extension) {
         case 'json':
             return json_decode($file, true);
-        case 'yaml':
-        case 'yml':
+        case 'yaml' || 'yml':
             return Yaml::parse($file);
         default:
             return throw new \Exception("Unknow file type {$extension}");
